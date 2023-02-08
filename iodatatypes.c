b97161272abca0b5cfd1e7d1e59e026a4576fffc
Chhavigoyal02 <114036963+Chhavigoyal02@users.noreply.github.com>
2023-02-08 20:21:01 +0530

    Add files via upload

---
+++ iodatatypes.c
@@ -0,0 +1,25 @@
+/******************************************************************************
+
+                            Online C Compiler.
+                Code, Compile, Run and Debug C program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <stdio.h>
+
+int main()
+{
+    char a;
+    printf("for character :\n");
+    scanf("%c",a);
+    int b;
+    printf("for number :\n");
+    scanf("%d",b);
+    float c;
+    printf("for decimal number :\n");
+    scanf("%f",c);
+    double d;
+    printf("for large numbers :\n");
+    scanf("%lf",b);
+}
