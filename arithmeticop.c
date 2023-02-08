d4f26a5a2cbceb3b39b563758701e57d617d570f
Chhavigoyal02 <114036963+Chhavigoyal02@users.noreply.github.com>
2023-02-08 20:52:45 +0530

    Add files via upload

---
+++ arithmeticop.c
@@ -0,0 +1,14 @@
+
+
+#include <stdio.h>
+
+int main()
+{
+    int a,b;
+    printf("enter numbers:\n");
+    scanf("%d%d",&a,&b);
+    printf("sum=%d\n",a+b);
+    printf("subtract=%d\n",a-b);
+    printf("multiply=%d\n",a*b);
+    printf("division=%d\n",a/b);
+}
