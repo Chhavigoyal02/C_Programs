99149c9dc236954b2303f662023da6958c8cbf88
Chhavigoyal02 <114036963+Chhavigoyal02@users.noreply.github.com>
2023-02-11 17:31:29 +0530

    Add files via upload

---
+++ fartocel.c
@@ -0,0 +1,11 @@
+
+
+#include <stdio.h>
+
+int main()
+{
+    int a;
+    printf("enter temperature in fahrenheit \n");
+    scanf("%d",&a);
+    printf("temperature in celsius is %f\n",(a-32)/1.8);
+}
