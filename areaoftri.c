523a27a3b56a1c044ca24b36d8072ef422379620
Chhavigoyal02 <114036963+Chhavigoyal02@users.noreply.github.com>
2023-02-11 17:44:28 +0530

    Add files via upload

---
+++ areaoftri.c
@@ -0,0 +1,13 @@
+
+
+#include <stdio.h>
+#include<math.h>
+void main()
+{
+    int a,b;
+    printf("enter base and height of triangle \n");
+    scanf("%d%d",&a,&b);
+    float c=(1/2.0)*a*b;
+    printf("result is : %f\n",c);
+}
+
