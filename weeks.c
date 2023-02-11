e56eef09ddcfec94b9f0b7d55d942d06306b41f1
Chhavigoyal02 <114036963+Chhavigoyal02@users.noreply.github.com>
2023-02-11 17:34:06 +0530

    Add files via upload

---
+++ weeks.c
@@ -0,0 +1,15 @@
+
+
+#include <stdio.h>
+
+int main()
+{
+    int a;
+    printf("enter number of days \n");
+    scanf("%d",&a);
+    int w=a/7;
+    int y=a/365;
+    printf("number of weeks is: %d\n",w);
+    printf("number of years is: %d\n",y);
+
+}
