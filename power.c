30c17463656a50816046eaf9db6bf17a7da949a6
Chhavigoyal02 <114036963+Chhavigoyal02@users.noreply.github.com>
2023-02-11 17:36:33 +0530

    Add files via upload

---
+++ power.c
@@ -0,0 +1,12 @@
+
+
+#include <stdio.h>
+#include<math.h>
+int main()
+{
+    int a,b;
+    printf("enter numbers \n");
+    scanf("%d%d",&a,&b);
+    int res=pow(a,b);
+    printf("result is : %d\n",res);
+}
