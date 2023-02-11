1a31b3d604dced4f8e13166ee3075c0ead0011ee
Chhavigoyal02 <114036963+Chhavigoyal02@users.noreply.github.com>
2023-02-11 17:38:03 +0530

    Add files via upload

---
+++ sqrt.c
@@ -0,0 +1,12 @@
+
+
+#include <stdio.h>
+#include<math.h>
+int main()
+{
+    int a;
+    printf("enter number \n");
+    scanf("%d",&a);
+    int res=sqrt(a);
+    printf("result is : %d\n",res);
+}
