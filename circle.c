c0b3d9cc3ddda624924153797824643008a66278
Chhavigoyal02 <114036963+Chhavigoyal02@users.noreply.github.com>
2023-02-11 17:22:42 +0530

    Add files via upload

---
+++ circle.c
@@ -0,0 +1,13 @@
+
+
+#include <stdio.h>
+
+int main()
+{
+    int a;
+    printf("enter radius of circle\n");
+    scanf("%d",&a);
+    printf("Diameter is %d\n",2*a);
+    printf("circumference is %f\n",2*3.14*a);
+    printf("area is %f\n",3.14*a*a);
+}
