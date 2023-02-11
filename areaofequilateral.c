64844589309670d05713159af8511274871a1d50
Chhavigoyal02 <114036963+Chhavigoyal02@users.noreply.github.com>
2023-02-11 17:46:35 +0530

    Add files via upload

---
+++ areaofequilateral.c
@@ -0,0 +1,13 @@
+
+
+#include <stdio.h>
+#include<math.h>
+void main()
+{
+    int a;
+    printf("enter side of triangle \n");
+    scanf("%d",&a);
+    float c=(1.71/4)*a*a;
+    printf("area is : %f\n",c);
+}
+
