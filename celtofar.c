e694578d33947a7743b506c42ac797756ce81ad2
Chhavigoyal02 <114036963+Chhavigoyal02@users.noreply.github.com>
2023-02-11 17:29:44 +0530

    Add files via upload

---
+++ celtofar.c
@@ -0,0 +1,11 @@
+
+
+#include <stdio.h>
+
+int main()
+{
+    int a;
+    printf("enter temperature in celcius \n");
+    scanf("%d",&a);
+    printf("temperature in fahrenheit is %f\n",(9.0/5*a)+32);
+}
